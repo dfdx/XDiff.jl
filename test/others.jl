895f8e9508e4dc0e6d67f6fd9774cb5521d63b9f
dfdx <faithlessfriend@gmail.com>
2017-09-03 15:36:31 +0300

    using sum_1(X[:, j]) and length(X[::]) notation

--- test/others.jl
+++ test/others.jl
@@ -1,3 +1,17 @@
 
 myfunc(x) = log(sum(x))
 test_compare(myfunc; x=rand(2))
+
+
+quote 
+    tmp685 = @get_or_create(mem, :tmp685, zero(Float64))
+    tmp686 = @get_or_create(mem, :tmp686, zero(Float64))
+    dtmp686!dx = @get_or_create(mem, :dtmp686!dx, zeros(Float64, (2,)))
+    dtmp686!dtmp686 = @get_or_create(mem, :dtmp686!dtmp686, zero(Float64))
+    tmp694 = @get_or_create(mem, :tmp694, zero(Float64))
+    dtmp686!dtmp686 = 1.0
+    tmp685 = sum(x)
+    dtmp686!dx .= dtmp686!dtmp686 .* (dtmp686!dtmp686 ./ tmp685)
+    tmp686 = log(tmp685)
+    tmp696 = (tmp686, dtmp686!dx)
+end
---
+++ test/vae.jl
@@ -0,0 +1,80 @@
+
+using Distributions
+
+function vae_cost(We1, be1, We2, be2, We3, be3, We4, be4, Wd1, bd1, Wd2, bd2, Wd3, bd3, eps, x)
+    dummy = 42.0
+    # encoder
+    he1 = tanh.(We1 * x .+ be1)
+    he2 = tanh.(We2 * he1 .+ be2)
+    mu = We3 * he2 .+ be3
+    log_sigma2 = We4 * he2 .+ be4
+    z = mu .+ sqrt.(exp.(log_sigma2)) .* eps
+    # decoder
+    hd1 = tanh.(Wd1 * z .+ bd1)
+    hd2 = tanh.(Wd2 * hd1 .+ bd2)
+    x_rec = logistic.(Wd3 * hd2 .+ bd3)
+    # loss
+    rec_loss = sum(x .* log.(1e-10 + x_rec) + (1 - x) .* log.(1e-10 + 1 - x_rec), 1)
+    latent_loss = -0.5 * sum(1 + log_sigma2 .- mu .^ 2 - exp.(log_sigma2), 1)
+    cost = mean(rec_loss .+ latent_loss)
+end
+
+function xavier_init(dim_in, dim_out; c=1)
+    low = -c * sqrt(6.0 / (dim_in + dim_out))
+    high = c * sqrt(6.0 / (dim_in + dim_out))
+    return rand(Uniform(low, high), dim_in, dim_out)
+end
+
+
+let
+    We1 = xavier_init(500, 784); be1 = randn(500);
+    We2 = xavier_init(500, 500); be2 = randn(500);
+    We3 = xavier_init(20, 500); be3 = randn(20);
+    We4 = xavier_init(20, 500); be4 = randn(20);
+    Wd1 = xavier_init(500, 20); bd1 = randn(500);
+    Wd2 = xavier_init(500, 500); bd2 = randn(500);
+    Wd3 = xavier_init(784, 500); bd3 = randn(784);
+    x = rand(784, 100); eps = rand(Normal(0, 1),  20)
+    inputs = [:We1 => We1, :be1 => be1, :We2 => We2, :be2 => be2,
+              :We3 => We3, :be3 => be3, :We4 => We4, :be4 => be4,
+              :Wd1 => Wd1, :bd1 => bd1, :Wd2 => Wd2, :bd2 => bd2,
+              :Wd3 => Wd3, :bd3 => bd3, :eps => eps, :x => x]
+
+    test_compare(vae_cost; inputs...)
+end
+
+
+# function xavier_init(dim_in, dim_out; c=1)
+#     low = -c * sqrt(6.0 / (dim_in + dim_out))
+#     high = c * sqrt(6.0 / (dim_in + dim_out))
+#     return rand(Uniform(low, high), dim_in, dim_out)
+# end
+
+
+# function vae_cost(We, Wd, x)
+#     z = We * x
+#     x_rec = exp.(Wd * z)
+#     rec_loss_mat = x .* log.(x_rec)
+#     rec_loss = sum(rec_loss_mat, 1)
+#     latent_loss = -0.5 * sum(z, 1)
+#     cost = mean(latent_loss .+ rec_loss)
+# end
+
+
+
+# let
+#     We = xavier_init(20, 784);
+#     Wd = xavier_init(784, 20);
+#     x = rand(784, 100); eps = rand(Normal(0, 1),  20)
+#     inputs = [:We => We,
+#               :Wd => Wd,
+#               :x => x]
+
+#     # rd_vals = rd_diff(vae_cost; inputs...)
+
+
+    
+    
+#     test_compare(vae_cost; inputs...)
+
+# end
